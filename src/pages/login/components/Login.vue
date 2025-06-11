f3e747814dd9510e1ecbd88039a96d1a4e7f43b2
Meng Sen <qyg2297248353@gmail.com>
2025-06-11 18:25:24 +0800

    优化登录页面

--- src/pages/login/components/Login.vue
+++ src/pages/login/components/Login.vue
@@ -7,6 +7,7 @@
     label-width="0"
     @submit="onSubmit"
   >
+    <!-- 账户登录 -->
     <template v-if="type == 'password'">
       <t-form-item name="account">
         <t-input v-model="formData.account" size="large" :placeholder="`${t('pages.login.input.account')}：admin`">
@@ -39,55 +40,18 @@
       </div>
     </template>
 
-    <!-- 扫码登录 -->
-    <template v-else-if="type == 'qrcode'">
-      <div class="tip-container">
-        <span class="tip">{{ t('pages.login.wechatLogin') }}</span>
-        <span class="refresh">{{ t('pages.login.refresh') }} <t-icon name="refresh" /> </span>
-      </div>
-      <qrcode-vue value="" :size="160" level="H" />
-    </template>
-
-    <!-- 手机号登录 -->
-    <template v-else>
-      <t-form-item name="phone">
-        <t-input v-model="formData.phone" size="large" :placeholder="t('pages.login.input.phone')">
-          <template #prefix-icon>
-            <t-icon name="mobile" />
-          </template>
-        </t-input>
-      </t-form-item>
-
-      <t-form-item class="verification-code" name="verifyCode">
-        <t-input v-model="formData.verifyCode" size="large" :placeholder="t('pages.login.input.verification')" />
-        <t-button size="large" variant="outline" :disabled="countDown > 0" @click="sendCode">
-          {{ countDown == 0 ? t('pages.login.sendVerification') : `${countDown}秒后可重发` }}
-        </t-button>
-      </t-form-item>
-    </template>
-
     <t-form-item v-if="type !== 'qrcode'" class="btn-container">
       <t-button block size="large" type="submit"> {{ t('pages.login.signIn') }} </t-button>
     </t-form-item>
-
-    <div class="switch-container">
-      <span v-if="type !== 'password'" class="tip" @click="switchType('password')">{{
-        t('pages.login.accountLogin')
-      }}</span>
-      <span v-if="type !== 'qrcode'" class="tip" @click="switchType('qrcode')">{{ t('pages.login.wechatLogin') }}</span>
-      <span v-if="type !== 'phone'" class="tip" @click="switchType('phone')">{{ t('pages.login.phoneLogin') }}</span>
-    </div>
   </t-form>
 </template>
 
 <script setup lang="ts">
-import QrcodeVue from 'qrcode.vue';
 import type { FormInstanceFunctions, FormRule, SubmitContext } from 'tdesign-vue-next';
 import { MessagePlugin } from 'tdesign-vue-next';
 import { ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 
-import { useCounter } from '@/hooks';
 import { t } from '@/locales';
 import { useUserStore } from '@/store';
 
@@ -114,25 +78,8 @@ const form = ref<FormInstanceFunctions>();
 const formData = ref({ ...INITIAL_DATA });
 const showPsw = ref(false);
 
-const [countDown, handleCounter] = useCounter();
-
-const switchType = (val: string) => {
-  type.value = val;
-};
-
 const router = useRouter();
 const route = useRoute();
-
-/**
- * 发送验证码
- */
-const sendCode = () => {
-  form.value.validate({ fields: ['phone'] }).then((e) => {
-    if (e === true) {
-      handleCounter();
-    }
-  });
-};
 
 const onSubmit = async (ctx: SubmitContext) => {
   if (ctx.validateResult === true) {
